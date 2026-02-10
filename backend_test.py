2a668066ada820cc8401cdf58d375047a4208159
Emergent Dev <dev@emergent.com>
2026-02-10 11:46:40 +0000

    auto-commit for df1a6e42-ccbb-4fcb-b027-7bdb39ca8e7f

--- backend_test.py
+++ backend_test.py
@@ -7,7 +7,7 @@ import time
 from datetime import datetime
 
 class LifeInsuranceAPITester:
-    def __init__(self, base_url="https://policy-logic.preview.emergentagent.com"):
+    def __init__(self, base_url="https://rule-processor-1.preview.emergentagent.com"):
         self.base_url = base_url
         self.tests_run = 0
         self.tests_passed = 0
